1dff0fc57641be18d748013d511839b66cb4a133
Melinda Walter <melinda_walter@hotmail.com>
2023-09-28 17:52:04 +0200

    new endpoints

---
+++ src/test/java/com/example/teststrategy/services/LoginServiceTest.java
@@ -0,0 +1,34 @@
+package com.example.teststrategy.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginServiceTest {
+
+    LoginService loginServiceTest;
+
+    @Test
+    void isEmail() {
+    }
+
+    @Test
+    void validatePasswordLength() {
+    }
+
+    @Test
+    void validateNameLength() {
+    }
+
+    @Test
+    void validateCapital() {
+    }
+
+    @Test
+    void validateSymbol() {
+    }
+
+    @Test
+    void validateAge() {
+    }
+}
