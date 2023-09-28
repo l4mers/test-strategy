d8330f81fee566a2e0422977925b3264ccfa9f76
l4mers <gustav@henrikssons.org>
2023-09-28 11:13:16 +0200

    init springboot

---
+++ src/main/java/com/example/teststrategy/TestStrategyApplication.java
@@ -0,0 +1,13 @@
+package com.example.teststrategy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestStrategyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestStrategyApplication.class, args);
+    }
+
+}
