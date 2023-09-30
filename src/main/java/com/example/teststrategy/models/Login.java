58e162bcae9ffb1ed3a8abcab9b3f0a7dfed868b
l4mers <gustav@henrikssons.org>
2023-09-30 17:57:01 +0200

    asd

--- src/main/java/com/example/teststrategy/models/Login.java
+++ src/main/java/com/example/teststrategy/models/Login.java
@@ -3,6 +3,7 @@ package com.example.teststrategy.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
